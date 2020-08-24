d55f990430cba1acc57830b65e1eafb6461d2b9c
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-08-24 18:01:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-// Add your code here
-
 configurationObject = {
   method: "POST",
   headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: /* Your data goes here */
+    "Content-Type": "application/json"
+  }
 };
