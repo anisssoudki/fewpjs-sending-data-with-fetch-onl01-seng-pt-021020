556f98453ba9e35ce853752ef8c71d6adda926ae
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-08-24 18:02:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-fetch("http://localhost:3000/dogs", {
+let formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle"
+};
+ 
+let configObj = {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json"
   },
-  body: JSON.stringify({
-    dogName: "Byron",
-    dogBreed: "Poodle"
-  })
-});
+  body: JSON.stringify(formData)
+};
+ 
+fetch("http://localhost:3000/dogs", configObj);
