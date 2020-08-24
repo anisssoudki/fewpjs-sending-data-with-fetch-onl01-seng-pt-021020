980b39c880e7c8adf08e21ec461ee918d69ae793
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-08-24 18:03:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-let formData = {
-  dogName: "Byron",
-  dogBreed: "Poodle"
-};
- 
-// method: "POST" is missing from the object below
-let configObj = {
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
-};
- 
-fetch("http://localhost:3000/dogs", configObj)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(object) {
-    console.log(object);
-  })
-  .catch(function(error) {
-    alert("Bad things! Ragnarők!");
-    console.log(error.message);
-  });
+unction submitData(userName, userEmail){
+
+    let formData = {
+        name: userName,
+        email: userEmail
+    };
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+    let request = fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        document.body.innerHTML = object[ "id" ];
+    })
+    .catch(function(error) {
+        alert("Unauthorized Access")
+        document.body.innerHTML = error.message
+    });
+    return request
+}
