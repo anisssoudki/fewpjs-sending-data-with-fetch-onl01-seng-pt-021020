ca14d2d25eb349043188f2de4b43cb22f8522242
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-08-24 18:02:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,4 +12,10 @@ let configObj = {
   body: JSON.stringify(formData)
 };
  
-fetch("http://localhost:3000/dogs", configObj);
+fetch("http://localhost:3000/dogs", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  });
